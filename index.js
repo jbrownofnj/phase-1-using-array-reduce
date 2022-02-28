6097eafba1fd00df59df162d653d79b66c78061b
John Brown <johnbrownsinbox@gmail.com>
2022-02-28 08:42:18 -0500

    commit tree bebbb035f795ebc384d33c156ac61d58b72e47d0

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+function returnsArrayTotal(anArray){const totalBatteries = batteryBatches.reduce((prev,curr)=>prev+curr,0);return anArray.reduce((prev,curr)=>prev+curr,0);}
+const totalBatteries = returnsArrayTotal(batteryBatches);
 
-// Code your solution here
